3c8a3702d8cefe2796054e33f4f78e8a6e3fb245
DESKTOP-FLUQDP6\DAEJANG <wlscndgh369@gmail.com>
2021-10-05 15:33:47 +0900

    gfgfg

--- app/src/main/java/com/hj/studyactivity/WriteActivity.java
+++ app/src/main/java/com/hj/studyactivity/WriteActivity.java
@@ -10,6 +10,6 @@ public class WriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
-        ;
+
     }
 }
